1b24b236cae2eca9a20af68ba3df1a3b5db25b96
Leonardo <leonardo@Leonardos-MacBook-Pro.local>
2024-02-08 00:37:07 -0300

    added light theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'misc/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,26 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Judge - Finanças Pessoais',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -81,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
@@ -115,11 +102,27 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: ElevatedButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+/*
+/* CSS HEX */
+--lapis-lazuli: #22577aff;
+--verdigris: #38a3a5ff;
+--emerald: #57cc99ff;
+--light-green: #80ed99ff;
+--tea-green: #c7f9ccff;
+
+/* CSS HEX *//
+--caf-noir: #4c2e05ff;
+--reseda-green: #7a8450ff;
+--delft-blue: #434371ff;
+--papaya-whip: #f9ecccff;
+--nyanza: #edf7d2ff;
+ */
+
---
+++ lib/misc/theme.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light(
+        primary: Color(0xff57cc99),
+        secondary: Color(0xff80ed99),
+        tertiary: Color(0xffc7f9cc),
+        background: Color(0xffedede9),
+        onPrimary: Color(0xff162149),
+        onSecondary: Color(0xff2A0834),
+        onBackground: Color(0xff22577a),
+        onTertiary: Color(0xff181818),
+        outline: Color(0xff1b263b),
+        error: Color(0xffbc4749),
+        surface: Color(0xffcbf3f0)),
+    fontFamily: 'Montserrat',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xff57cc99),
+        foregroundColor: const Color(0xff162149),
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    ));
+
+final darkTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light(
+        primary: Color(0xff57cc99),
+        secondary: Color(0xff80ed99),
+        tertiary: Color(0xffc7f9cc),
+        background: Color(0xffedede9),
+        onPrimary: Color(0xff162149),
+        onSecondary: Color(0xff2A0834),
+        onBackground: Color(0xff22577a),
+        onTertiary: Color(0xff181818),
+        outline: Color(0xff1b263b),
+        error: Color(0xffbc4749),
+        surface: Color(0xffcbf3f0)),
+    fontFamily: 'Montserrat',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xff57cc99),
+        foregroundColor: const Color(0xff162149),
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    ));
